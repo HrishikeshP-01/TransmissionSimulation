126a6486be1b05d06b5277a8a0bf0a1e12938ffa
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-09-10 15:37:16 +0530

    Created intial locomotion script

--- Assets/Scripts/Locomotion.cs
+++ Assets/Scripts/Locomotion.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Locomotion : MonoBehaviour
 {
+    NavMeshAgent agent;
+    [SerializeField]
+    private Transform destination;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination.position);
     }
 
     // Update is called once per frame
